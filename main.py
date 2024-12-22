df8a84275b0abd0c3ba22bb75ed69695c02e6869
boevoyogurec <pvoit2006@gmail.com>
2024-12-22 20:18:31 +0300

    написал часть main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# main.py - стартовый модуль проекта
- 
-# Импорты из модуля
- 
+from functions import *
 
-# Функция запуска импортированных функций
-def main():
-   pass()
-# Инициализационный скрипт
-if name == "main":
-    main()
+df = input_excel_file('phone_numbers.xlsx') # изменить на свой путь
+# Указываем имя столбца с номерами телефонов
+phone_column_name = 'phone_number'
+
+# Проверяем наличие столбца
+if phone_column_name not in df.columns:
+    print(f"Столбец '{phone_column_name}' отсутствует в файле.")
+else:
+    # Очищаем номера телефонов
+    df[phone_column_name] = df[phone_column_name].apply(clean_phone_number)
