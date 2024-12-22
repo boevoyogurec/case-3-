59c1ae9d36017ec4e91db78653b83aa17297b353
Aoxiz <dim.chepurnov@gmail.com>
2024-12-22 21:00:54 +0300

    написал функцию input_excel_file

--- finction.py
+++ finction.py
@@ -1,7 +1,9 @@
 # func.py - модуль для импорта функций для решения задачи
 # TODO Добавить импорты внешних зависимостей
 # import <название зависимости>
-def func1():
-    pass
-def func2():
-    pass
+def input_excel_file(file_path): 
+    # Загружаем исходный файл 
+    input_file = file_path 
+    # Читаем его из excrl 
+    df = pd.read_excel(input_file) 
+    return df
